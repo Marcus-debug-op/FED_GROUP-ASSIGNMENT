ba9d2cd0b4943a94625bb431b48e78ffb7938028
bouttimegit <timelyloh@gmail.com>
2026-02-02 22:12:24 +0800

    SignIn/SignUp pages integration with firebase activated

---
+++ firebase-init.js
@@ -0,0 +1,19 @@
+// js/firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDo8B0OLtAj-Upfz7yNFeGz4cx3KWLZLuQ",
+  authDomain: "hawkerhub-64e2d.firebaseapp.com",
+  databaseURL: "https://hawkerhub-64e2d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hawkerhub-64e2d",
+  storageBucket: "hawkerhub-64e2d.firebasestorage.app",
+  messagingSenderId: "722888051277",
+  appId: "1:722888051277:web:59926d0a54ae0e4fe36a04"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getDatabase(app);
